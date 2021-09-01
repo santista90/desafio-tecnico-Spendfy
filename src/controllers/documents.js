ca60185817ae22c5d17d7993f52919b95f0d7215
Luiz Felipe <luizfelipe90@hotmail.com>
2021-08-31 21:29:21 -0300

    Criado função para cadastrar documento

---
+++ src/controllers/documents.js
@@ -0,0 +1,31 @@
+const knex = require('../config');
+
+const registerDocument = async (req, res) => {
+    const { kbsize, name, content } = req.body;
+
+    const dateNow = new Date().toLocaleDateString();
+
+    try {
+
+        const documentExist = await knex('documents').where({ name }).first();
+
+        if (documentExist) {
+            return res.status(400).json("Já existe um documento com este nome");
+        }
+
+        const document = await knex('documents').insert({ kbsize, name, content, 'createdat': dateNow }).returning('*');
+
+        if (document.length === 0) {
+            return res.status(400).json("Não foi possível cadastrar o documento.");
+        }
+
+        return res.status(200).json('Documento foi cadastrado com sucesso.');
+
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
+
+module.exports = {
+    registerDocument,
+}
